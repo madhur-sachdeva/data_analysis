c2d55922dba322568b2e06791ef65271affb3bd7
Madhur Sachdeva <madhursachdeva@Madhurs-MacBook-Air.local>
2024-10-05 19:40:22 +0530

    assignment 1

---
+++ python_namaste_sql/assignment_1.py
@@ -0,0 +1,17 @@
+# input function
+# name=input("enter your name: ")
+# print(name)
+# -----------------------------------------------------
+# 1- write a program which takes 2 inputs from the user : 
+# weight(kg) and height(meter) and prints the BMI in the output.
+
+# BMI = weight / (square of height)
+
+weight = int(input())
+height = float(input())
+
+BMI = weight / (height * height)
+print(round(BMI,2))
+
+# -----------------------------------------------------
+
